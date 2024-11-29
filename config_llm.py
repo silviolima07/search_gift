49566a8844efc0380b201604baf1eae2537f44c1
Silvio Lima <silviolima07@gmail.com>
2024-11-29 19:21:42 -0300

    Update

---
+++ config_llm.py
@@ -0,0 +1,17 @@
+# config.py
+import os
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+from groq import Groq
+
+# Carregar variáveis de ambiente
+load_dotenv()
+
+
+
+# Obter a chave da API GROQ
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+
+llama = "/groq/llama3-70b-8192",
+           
