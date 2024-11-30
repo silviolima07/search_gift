b178228354fdfc951a9a7f9ee6aacbe5e8ada0d7
Silvio Lima <silviolima07@gmail.com>
2024-11-30 12:42:12 -0300

    update

--- my_agents.py
+++ my_agents.py
@@ -4,7 +4,7 @@ import crewai_tools
 
 from crewai_tools import SerperDevTool
 
-#from MyLLM import MyLLM
+from MyLLM import MyLLM
 from config_llm import llama, gpt
 
 from dotenv import load_dotenv
@@ -13,15 +13,15 @@ load_dotenv()
 
 import os
 
-load_dotenv()
-
 # Obter a chave da API GROQ
 GROQ_API_KEY = os.getenv('GROQ_API_KEY')
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # Definir o modelo de linguagem
-llm = llama
+
+llm = MyLLM.GPT4o_mini #GROQ_LLAMA
+
 
 
 
