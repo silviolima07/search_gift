a2568470a5d439b1d3b4edc9882e502b4837724f
Silvio Lima <silviolima07@gmail.com>
2024-11-30 16:24:56 -0300

    Update

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+OPENAI_API_KEY = os.getenv('OPENAI')
 
 st.write("OPENAI_API_KEY:", OPENAI_API_KEY)
 
