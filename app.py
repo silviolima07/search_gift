82eabe3cca5c49f640a25ed8d5024b710aac7f87
Silvio Lima <silviolima07@gmail.com>
2024-12-01 12:04:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -216,10 +216,7 @@ if option == 'Pesquisar':
         tempo_restante = timedelta(minutes=1) - (datetime.now() - st.session_state.ultimo_click)
         segundos_restantes = int(tempo_restante.total_seconds())
         st.warning(f"O botão será habilitado novamente em {segundos_restantes} segundos.")
-        for n in range(1, segundos_restantes):
-            st.write(n)
-
-
+        
     ##################################
     """
     if st.button("INICIAR"):
