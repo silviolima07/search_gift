000204880ed957a71aaa094841864a85e16147cb
Silvio Lima <silviolima07@gmail.com>
2024-12-01 11:58:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -216,6 +216,7 @@ if option == 'Pesquisar':
         tempo_restante = timedelta(minutes=1) - (datetime.now() - st.session_state.ultimo_click)
         segundos_restantes = int(tempo_restante.total_seconds())
         st.warning(f"O botão será habilitado novamente em {segundos_restantes} segundos.")
+        st.warning(f"Clique em algumas das opções depois do tempo passar.")
 
 
     ##################################
