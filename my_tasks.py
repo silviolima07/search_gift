88cd7b727ce2f6d37bb50ffcca75b8eca0f1c9d4
Silvio Lima <silviolima07@gmail.com>
2024-12-02 15:38:24 -0300

    Update my_tasks.py

--- my_tasks.py
+++ my_tasks.py
@@ -58,7 +58,8 @@ serper_tool.cache_function = funcao_cache
 recomendar = Task(
     description =""" 
 Use o SerperDevTool para pesquisar somente no site {site} do Brasil. 
-Encontre e recomende no maximo {qtd} presente {tipo} para {genero}, com valor abaixo de {preco}. 
+Encontre e recomende no maximo {qtd} presente {tipo} para {genero}, com valor abaixo de {preco}.
+Resposta deve conter {qtd} items.
 Não mostrar resposta na console, apenas salve resultado numa lista formato Markdown.
 """ ,
     expected_output =
