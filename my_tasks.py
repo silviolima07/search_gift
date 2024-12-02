d2acf17e04ec4f5a4628a13ec85ef37a43e9b8cd
Silvio Lima <silviolima07@gmail.com>
2024-12-02 11:51:21 -0300

    Update my_tasks.py

--- my_tasks.py
+++ my_tasks.py
@@ -31,6 +31,29 @@ SERPER_API_KEY = os.getenv('SERPER_API_KEY')
 serper_tool = SerperDevTool()
 serper_tool.n_results = 10
 
+
+# Dicionario para rastrear as últimas consultas de cada termo
+ultima_consulta_busca = {}
+# Fun ção de cache personalizada
+def funcao_cache ( args , resultado ) :
+    termo = args[0] # Primeiro argumento é o termo de busca
+    agora = datetime.now ()
+    
+    # Verifica se o termo foi consultado nos últimos 60 minutos
+    if termo in ultima_consulta_busca :
+        ultima_hora_consulta = ultima_consulta_busca [ termo ]
+        if agora - ultima_hora_consulta < timedelta ( hours =1) :
+            print("Usar cache")
+            return True # cache se a última consulta foi menos de 1h
+    ultima_consulta_busca [ termo ] = agora # última consulta
+    print('Não usar cache')
+    return False # Não armazena em cache se passou mais de 1 hora
+
+
+# Associa a função de cache à ferramenta SerperDevTool
+serper_tool.cache_function = funcao_cache
+
+
 # Configuração da Task
 recomendar = Task(
     description =""" 
