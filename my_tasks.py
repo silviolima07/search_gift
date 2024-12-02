50596e44f1074d5d21ee26103d738133db3c1716
Silvio Lima <silviolima07@gmail.com>
2024-12-02 16:07:41 -0300

    Update my_tasks.py

--- my_tasks.py
+++ my_tasks.py
@@ -63,10 +63,10 @@ Resposta deve conter no máximo {qtd} items.
 Não mostrar resposta na console, apenas salve resultado numa lista formato Markdown.
 """ ,
     expected_output =
-    """\ 
-        *** Nome: Nome do presente\n
-        *** Descrição: Breve descrição do presente\n
-        *** Preço: Valor do presente
+    f"""\ 
+        *** Nome: {Nome do presente}\n
+        *** Descrição: {Breve descrição do presente}\n
+        *** Preço: {Valor do presente}
     """,
     agent=guia_compras,
     tools=[serper_tool],  # Ferramenta configurada
